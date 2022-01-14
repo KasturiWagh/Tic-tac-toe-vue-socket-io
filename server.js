193a3e9707f992ba0f83374cf304b1ea8ff99804
kasturi wagh <kasturiwagh9@gmail.com>
2022-01-15 02:42:42 +0530

    added .get

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ const app = express();
 app.use(express.static(__dirname + "/dist/"));
 app.get(/.*/, function(req, res) {
   res.sendFile(__dirname + "/dist/index.html");
+  res.setHeader("Access-Control-Allow-Origin", "*");
 });
 app.listen(port);
 
